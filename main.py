7e0bbefa079c23f491fc82bd35051d4d824f3a75
Nivastej <63730345+Nivastej@users.noreply.github.com>
2023-07-23 23:29:09 +0000

    updated

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': "Java developer",
+        'location': "remote",
+        'salary': "Rs.19,00,000 LPA"
+           
+    },
+    {
+        'id': 2,
+        'title': "Developer",
+        'location': "Hyderabad",
+        'salary': "Rs.2,00,000"
+         
+    },
+  {
+        'id': 3,
+        'title': "Data scientist",
+        'location': "New York"
+      
+         
+    },
+]
 @app.route('/')
 
 
 def home():
-    return render_template('index.html')
+    return render_template('index.html',jobs=JOBS)
+
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
   
 
 if __name__ == '__main__':
